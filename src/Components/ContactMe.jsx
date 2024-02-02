ad55671c7de49b347a40089fad76f3e66ecbc515
Jose M Serra Jr <joseserra803@gmail.com>
2024-02-01 19:16:14 -0500

    Adjusted Contact Form.

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -3,29 +3,8 @@ import React from 'react';
 const ContactMe = () => {
   return (
       <div className="container">
-          <p>
-            For all inquries as well please fill out this form I will try to contact you within 2 business days.
-          </p>
-          <form method="POST" action="mailto: joseserra803@gmail.com"
-              enctype="multipart/form-data">
-              <div className="control">
-                  Name:
-                  <input aria-required="" id="name" type="text" />
-              </div>
-                  <div className="control">
-                      Phone Number:
-                      <input aria-required="" id="mobile_number" type="tel" />
-                  </div>
-                      <div className="control">
-                          Body:
-                          <textarea rows="7" cols="30" name="comment">
-                          </textarea>
-                      </div>
-                          <div className="control">
-                              <input type="submit" value="Submit" />
-                          </div>
-             </form>
-          </div>
+          <iframe align="center" src="https://docs.google.com/forms/d/e/1FAIpQLSfo1i731bV7PCUZFbARj2yna7p1ZUWwvQN5hm6WXro3Cokhpg/viewform?embedded=true" width="640" height="917" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>    
+      </div>
   );
 }
 
